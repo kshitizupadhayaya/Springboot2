0872b6aa8748672f66fb349a9aa2ec7ae8e3e89a
kshitizupadhayaya <kxupadhayaya@gmail.com>
2024-04-20 15:12:10 +0545

    Commit fourth

---
+++ src/main/java/com/web/demo1/Demo1Application.java
@@ -0,0 +1,22 @@
+package com.web.demo1;
+
+import com.web.demo1.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+
+
+public class Demo1Application
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+}
